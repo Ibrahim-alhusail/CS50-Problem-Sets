2419df9342d5eb8001e737a93d6098e1c19b7b2b
= <=>
2026-05-13 12:30:33 +0300

    Problem Set 0 Done :)

---
+++ Problem-Set-0/faces/faces.py
@@ -0,0 +1,31 @@
+'''
+Making Faces
+
+Before there were emoji, there were emoticons, whereby text like :) was a happy face and text like :( was a sad face.
+Nowadays, programs tend to convert emoticons to emoji automatically!
+
+In a file called faces.py, implement a function called convert that accepts a str as input and returns that same input with any :)
+converted to 🙂 (otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face).
+All other text should be returned unchanged.
+
+Then, in that same file, implement a function called main that prompts the user for input,
+calls convert on that input, and prints the result. You’re welcome, but not required,
+to prompt the user explicitly, as by passing a str of your own as an argument to input. Be sure to call main at the bottom of your file.
+'''
+
+def main():
+    arg = str(input("Enter any input you want:\n> ")).strip()
+    print(arg)
+    convert(arg)
+
+def convert(arg):
+    sentance = []
+    for word in arg.split(' '):
+        if word == ':)':
+            word = '🙂'
+        elif word == ':(':
+            word = '🙁'
+        sentance.append(word)
+    print(' '.join(sentance))
+main()
+
